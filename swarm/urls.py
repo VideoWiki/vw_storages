9d95c2a1130e943498ea4ae5f86343b5e1de9409
Amanb1145 <amanb1145@gmail.com>
2024-01-13 18:59:48 +0530

    added sia apis

--- swarm/urls.py
+++ swarm/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from swarm.views import FileUploadAPI, FileUploadStatusAPI, FileDownloadAPI, FileDownloadStatusAPI
+from swarm.views import FileUploadAPI, FileUploadStatusAPI, FileDownloadAPI, FileDownloadStatusAPI, VideoUploadViewSIA, VideoDownloadViewSIA
 
 urlpatterns = [
     path('swarm/upload/', FileUploadAPI.as_view(), name='swarm-file-upload'),
     path('swarm/upload/status/<str:task_id>/', FileUploadStatusAPI.as_view(), name='swarm-file-upload-status'),
     path('swarm/download/', FileDownloadAPI.as_view(), name='swarm-file-download'),
     path('download/status/<str:task_id>/', FileDownloadStatusAPI.as_view(), name='file-download-status'),
+    path('sia/upload/', VideoUploadViewSIA.as_view(), name='sia-file-upload'),
+    path('sia/download/', VideoDownloadViewSIA.as_view(), name='sia-file-upload'),
 
 ]
 
--- swarm/views.py
+++ swarm/views.py
@@ -11,6 +11,7 @@ import requests
 from subprocess import PIPE, Popen
 from dotenv import load_dotenv
 from vw_storages.settings import BASE_DIR
+from django.http import JsonResponse, HttpResponse
 
 
 load_dotenv()
@@ -99,3 +100,52 @@ class FileDownloadStatusAPI(APIView):
                 return Response(result, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         else:
             return Response({"status": "Task is still in progress."}, status=status.HTTP_202_ACCEPTED)
+
+
+#SIA
+
+
+class VideoUploadViewSIA(APIView):
+    def post(self, request, *args, **kwargs):
+
+        # Download file contents as binary
+        file_url = request.data['file_url']
+        parsed_url = urlparse(file_url)
+        filename = parsed_url.path.split("/")[-1]
+        url = f"https://storage.sia.video.wiki/api/worker/objects/videowiki/{filename}"
+
+        resp = requests.get(file_url)
+        binary_data = resp.content
+
+        headers = {
+            'Content-Type': 'video/webm',
+            'Authorization': 'Basic OnBhc3N3b3Jk'
+        }
+
+        response = requests.put(url, headers=headers, data=binary_data)
+
+        return Response(response.text, status=response.status_code)
+
+
+class VideoDownloadViewSIA(APIView):
+    def get(self, request, *args, **kwargs):
+        file_name = request.data.get('file_name')
+
+        headers = {
+            'Authorization': 'Basic OnBhc3N3b3Jk',
+            'Content-Type': 'video/webm'
+        }
+        url = f"https://storage.sia.video.wiki/api/worker/objects/videowiki/{file_name}"
+
+        # Send GET request
+        response = requests.get(url, headers=headers)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            binary_data = response.content
+
+            return HttpResponse(binary_data, content_type='video/webm')
+        else:
+            return JsonResponse({'error': 'Failed to download file'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
