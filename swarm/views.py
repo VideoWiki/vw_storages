73fe2d8a83b18bef29fdc124089a811ab5d31a71
Aman Bishnoi <amanb1145@gmail.com>
2024-07-04 10:06:19 +0530

    Update views.py

--- swarm/views.py
+++ swarm/views.py
@@ -113,8 +113,8 @@ class VideoUploadViewSIA(APIView):
         file_url = request.data['file_url']
         parsed_url = urlparse(file_url)
         filename = parsed_url.path.split("/")[-1]
-        if file_url.startswith("https://live1.decast.live"):
-            filename = filename + ".webm"
+        # if file_url.startswith("https://live1.decast.live"):
+        #     filename = filename + ".webm"
         url = f"https://storage.sia.video.wiki/api/worker/objects/videowiki/{filename}"
 
         resp = requests.get(file_url)
