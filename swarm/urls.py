cf8a0b386121f3d6ed26c191b4f390951397953f
Aman Bishnoi <amanb1145@gmail.com>
2024-07-12 13:54:23 +0530

    Update urls.py

--- swarm/urls.py
+++ swarm/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from swarm.views import FileUploadAPI, FileUploadStatusAPI, FileDownloadAPI, FileDownloadStatusAPI, VideoUploadViewSIA, VideoDownloadViewSIA, StartUploadViewSwarm, TaskStatusViewSwarm, VideoUploadViewSIA, VideoDownloadViewSIA, TaskStatusView
+from swarm.views import FileUploadAPI, FileUploadStatusAPI, FileDownloadAPI, FileDownloadStatusAPI, VideoUploadViewSIA, VideoDownloadViewSIA, StartUploadViewSwarm, TaskStatusViewSwarm, VideoUploadViewSIA, VideoDownloadViewSIAC, TaskStatusView
 
 urlpatterns = [
     path('swarm/upload/', FileUploadAPI.as_view(), name='swarm-file-upload'),
@@ -11,7 +11,7 @@ urlpatterns = [
     path('swarm-upload/', StartUploadViewSwarm.as_view(), name='swarm-file-upload'),
     path('swarm-status/<str:task_id>/', TaskStatusViewSwarm.as_view(), name='swarm-file-upload-status'),
     path('upload/sia/', VideoUploadViewSIA.as_view(), name='upload_video'),
-    path('download/sia/', VideoDownloadViewSIA.as_view(), name='download_video'),
+    path('download/sia/', VideoDownloadViewSIAC.as_view(), name='download_video'),
     path('sia-status/<str:task_id>/', TaskStatusView.as_view(), name='task_status'),
 ]
 
